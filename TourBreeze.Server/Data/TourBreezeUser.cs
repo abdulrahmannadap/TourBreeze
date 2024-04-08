274b15f0112352695aaee18941b22bf51841b8cb
Abdul Rahman <63226271+abdulrahmannadap@users.noreply.github.com>
2024-04-08 17:43:24 +0530

    Update Identity Page Login Logout Page Funstality

---
+++ TourBreeze.Server/Data/TourBreezeUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace TourBreeze.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the TourBreezeUser class
+public class TourBreezeUser : IdentityUser
+{
+}
+
--- TourBreeze/Program.cs
+++ TourBreeze/Program.cs
@@ -1,6 +1,15 @@
 using TourBreeze;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TourBreeze.Data;
+using TourBreeze.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("TourBreezeContextConnection") ?? throw new InvalidOperationException("Connection string 'TourBreezeContextConnection' not found.");
+
+builder.Services.AddDbContext<TourBreezeContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<TourBreezeUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<TourBreezeContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -32,6 +41,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{area=Admin}/{controller=Products}/{action=ProductList}/{id?}");
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
+app.MapRazorPages();
 app.Run();
